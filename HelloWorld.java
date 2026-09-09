f66a26b07a994b36d7b159858379bcb6c2801352
tangzhenyu69-ux <tangzhenyu69@gmail.com>
2026-09-09 22:08:26 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package a;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        输出英文HelloWorld
+/*        输出英文HelloWorld
+          输出英文HelloWorld*/
+        System.out.println("hello world");
+    }
+}
